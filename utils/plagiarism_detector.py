85feb93279d8314c832c8d443156dc4693bf5ee0
Anju Rajesh <anjurajesh7214@gmail.com>
2026-02-13 12:00:00 +0530

    commit

--- utils/plagiarism_detector.py
+++ utils/plagiarism_detector.py
@@ -5,8 +5,6 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-
-
 # Ensure NLTK data is available
 try:
     nltk.data.find('tokenizers/punkt')
